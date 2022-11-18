da74315196346d05a04b9eb0a38ba18b80df0e6d
jvillane <jvillane@gmail.com>
2022-11-18 13:45:58 -0300

    ADD: Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,30 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from "next/head";
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Trasgo Mueblería | calidad de diseño e implementación</title>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=UA-249916534-1"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-249916534-1');
+        `}
+        </Script>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp
